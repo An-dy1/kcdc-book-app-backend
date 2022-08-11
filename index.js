d52c1effaf0b0b3eefaeb71c4418620dfc327fc7
Andy <andrea_sue2@icloud.com>
2022-08-11 14:48:40 -0500

    use dotenv to store mongo creds

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const { request } = require('express');
+require('dotenv').config();
 
 const PORT = process.env.PORT || 5005;
-let data = [{
-    title: 'Normal People',
-    url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwFHgvzFonpSXs_uQRIVnUDG9oN76uLo-7nA&usqp=CAU',
-}, ];
 
 let collection;
 
@@ -21,27 +18,22 @@ const client = new MongoClient(uri, {
 
 client.connect((err) => {
     collection = client.db('book-app').collection('books');
-    // perform actions on the collection object
-    // client.close();
 });
 
 let app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/data', async(req, res) => {
+app.get('/books', async(req, res) => {
     let data = await collection.find({}).toArray();
-    console.log(data);
     res.send(data).status(200);
 });
 
-app.get('/data/:index', async(req, res) => {
+app.get('/books/:index', async(req, res) => {
     res.send({ data: data[req.params.index] }).status(200);
 });
 
-app.post('/book', async(req, res) => {
-    // await connectToMongo();
-    // data.push(req.body);
+app.post('/books', async(req, res) => {
     let result = await collection.insertOne(req.body);
     res.send(result).status(201);
 });
