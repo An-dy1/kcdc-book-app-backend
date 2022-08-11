7bed32c926f68d4cbf0276590ae991b83acd96f4
Andy <andrea_sue2@icloud.com>
2022-08-11 14:16:42 -0500

    install dotenv and move creds for local db out

--- index.js
+++ index.js
@@ -8,11 +8,30 @@ let data = [{
     url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwFHgvzFonpSXs_uQRIVnUDG9oN76uLo-7nA&usqp=CAU',
 }, ];
 
+let collection;
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://an-dy1:${process.env.MONGO_BOOKS_DB_PASSWORD}@cluster0.s1vyspe.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+client.connect((err) => {
+    collection = client.db('book-app').collection('books');
+    // perform actions on the collection object
+    // client.close();
+});
+
 let app = express();
 app.use(cors());
 app.use(express.json());
 
 app.get('/data', async(req, res) => {
+    let data = await collection.find({}).toArray();
+    console.log(data);
     res.send(data).status(200);
 });
 
@@ -21,8 +40,10 @@ app.get('/data/:index', async(req, res) => {
 });
 
 app.post('/book', async(req, res) => {
-    data.push(req.body);
-    res.send('hello!').status(201);
+    // await connectToMongo();
+    // data.push(req.body);
+    let result = await collection.insertOne(req.body);
+    res.send(result).status(201);
 });
 
 app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
